a59cedea8cf7e81fb703b5dc4d5d5561bb46d617
Daniel Scheffler <danschef@gfz.de>
2026-03-02 19:53:18 +0100

    Keep the build script for non-cross-compilation unchanged and just use another one for cross-compilation.

--- recipe/build.bat
+++
@@ -1,8 +0,0 @@
-@echo on
-
-:: ensure host python is found first
-set "PATH=%PREFIX%;%PREFIX%\Library\bin;%PATH%"
-
-meson setup builddir %MESON_ARGS%
-meson compile -C builddir
-meson install -C builddir
--- recipe/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-# ensure host python is found first
-export PATH="$PREFIX/bin:$PATH"
-
-meson setup builddir $MESON_ARGS
-meson compile -C builddir
-meson install -C builddir
